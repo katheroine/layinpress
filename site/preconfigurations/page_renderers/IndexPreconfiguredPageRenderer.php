6be0f406b1bfb10d324239f759fb4ff09acaa0c8
katheroine <katheroine@gmail.com>
2023-04-08 19:59:04 +0200

    Layout configurability implemented [katheroine#17]

--- site/preconfigurations/page_renderers/IndexPreconfiguredPageRenderer.php
+++ site/preconfigurations/page_renderers/IndexPreconfiguredPageRenderer.php
@@ -11,21 +11,20 @@
 
 namespace Katheroine\LayinPress\Preconfiguration;
 
+
+
 class IndexPreconfiguredPageRenderer extends AbstractBasePreconfiguredPageRenderer
 {
-    protected const WP_RELATIVE_PATH = __DIR__ . '/../../../../..';
-    protected const LAYIN_RELATIVE_PATH = __DIR__ . '/../../..';
+    use ConfigurableTrait;
 
     protected function providePreconfiguration(): array
     {
-        $relativePath = __DIR__ . '/../../..';
-
         return [
             'templates_dir_path' => './site/templates/',
             'template_subdir_path' => '',
             'template_file_extension' => 'timber.html',
             'page_file_extension' => 'php',
-            'site_config_path' => $relativePath . '/site/config/site_config.yaml',
+            'site_config_path' => $this->provideSiteConfigPath(),
             'navigation_links_config_path' => $this->provideNavigationLinksConfigPath(),
             'contact_info_links_config_path' => $this->provideContactInfoLinksConfigPath(),
             'base_url' => '../..',
@@ -33,28 +32,5 @@ class IndexPreconfiguredPageRenderer extends AbstractBasePreconfiguredPageRender
             'assets_dir_path' => '/wp-content/themes/layinpress/site/public/assets',
             'is_debug_mode' => false,
         ];
-    }
-
-    protected function provideNavigationLinksConfigPath(): string
-    {
-        return $this->provideConfigPath('navigation_links');
-    }
-
-    protected function provideContactInfoLinksConfigPath(): string
-    {
-        return $this->provideConfigPath('contact_info_links');
-    }
-
-    protected function provideConfigPath(string $configName): string
-    {
-        $configPath = self::WP_RELATIVE_PATH . '/uploads/' . $configName . '.txt';
-
-        if (file_exists($configPath)) {
-            return $configPath;
-        }
-
-        $configPath = self::LAYIN_RELATIVE_PATH . '/site/config/' . $configName . '.yaml';
-
-        return $configPath;
     }
 }
